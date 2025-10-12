088b3d1df5a24430b0d2117ef3d4ee01670b8ca8
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2025-10-11 19:52:53 -0700

    Simplify door logic
    The interaction is always InteriorLoadTrigger. So parent chain scan isnt needed,

--- TEMPLATE.cs
+++ TEMPLATE.cs
@@ -2,9 +2,6 @@ using MelonLoader;
 using UnityEngine;
 using Il2CppTLD;
 using Il2CppTLD.Interactions;
-using System.Collections.Generic;
-
-[assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace DisableDoors
 {
@@ -25,30 +22,17 @@ namespace DisableDoors
             {
                 GameObject obj = interaction.gameObject;
 
-                // Walk parent chain to see if it’s a door
-                Transform current = obj.transform;
-                bool isDoor = false;
-
-                while (current != null)
+                foreach (string keyword in DoorKeywords)
                 {
-                    foreach (string keyword in DoorKeywords)
+                    if (current.name.ToLower().Contains(keyword.ToLower()))
                     {
-                        if (current.name.ToLower().Contains(keyword.ToLower()))
-                        {
-                            isDoor = true;
-                            break;
-                        }
+                        interaction.enabled = false;
+                        break;
                     }
-                    if (isDoor) break;
-                    current = current.parent;
-                }
-                if (isDoor)
-                {
-                    // Disable only the interaction component
-                    interaction.enabled = false;
                 }
             }
         }
     }
 }
+
 
