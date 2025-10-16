87859401b0a005c9d132ed8d015cdaeae4475591
Corwin <30555096+Corwin-dev@users.noreply.github.com>
2025-10-15 19:21:59 -0700

    refactored

---
+++ DisableDoors.cs
@@ -0,0 +1,41 @@
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+using Il2CppTLD.Interactions;
+using ModSettings;
+
+namespace DisableDoors
+{
+    internal class DisableDoorsSettings : JsonModSettings
+    {
+        internal static DisableDoorsSettings? Instance;
+        [Name("Disable Doors")]
+        [Description("Enables the mod")]
+        public bool DisableDoors = true;
+    }
+    public class Main : MelonMod 
+    {
+        public override void OnInitializeMelon()
+        {
+            DisableDoorsSettings.Instance = new DisableDoorsSettings(); 
+            DisableDoorsSettings.Instance.AddToModSettings("Disable Doors");
+        }
+    }
+
+    [HarmonyPatch(typeof(TimedHoldInteraction), "PerformHold")]
+    public static class Patch_TimedHoldInteraction_PerformHold
+    {
+        static bool Prefix(TimedHoldInteraction __instance)
+        {
+            if (__instance == null || __instance.gameObject == null || DisableDoorsSettings.Instance == null)
+                return true;
+
+            if (DisableDoorsSettings.Instance.DisableDoors && __instance.gameObject.transform.name == "InteriorLoadTrigger")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
